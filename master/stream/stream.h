7f9c09f16fe6709dcb533030245886a60cb1692d
changnet <940477049@qq.com>
2016-04-08 20:13:32 +0800

    binary stream library

--- master/stream/stream.h
+++ master/stream/stream.h
@@ -56,6 +56,67 @@ public:
 
     int32 tag_array_end();
     int32 tag_array_begin( const char *key );
+public:
+    class io
+    {
+        explicit io( const char *buffer,uint32 size )
+            : _buffer(buffer),_size(size),_offset(0)
+        {
+            assert( "stream:illeage io buffer",_buffer && _size > 0 );
+        }
+        public:
+#if defined(__i386__) || defined(__x86_64__)
+
+/* LDR/STR 对应汇编指令LDR/STR */
+# define LDR(_dest,_src,_t) _dest = (*reinterpret_cast<const _t *>(_p))
+
+#else
+
+# define LDR(_dest,_src,_t) memcpy( &_dest,_src,sizeof t )
+
+#endif
+            inline operator_t &operator >> ( int8 &val )
+            {
+                static uint32 offset = sizeof( int8 );
+                if ( _offset + offset > _size ) /* overflow */
+                {
+                    assert( "stream:io read int8 overflow",false );
+                    return *this;
+                }
+
+                /* val = *reinterpret_cast<const int8*>( buffer + _offset );
+                 * 使用reinterpret_cast速度更快，但可能会出现内存对齐问题，尤其是
+                 * 在编译时指定了对齐的情况下，类型强制转换后导致编译器
+                 * memcpy 在O3优化下速度与reinterpret_cast基本相当
+                 */
+                memcpy( &val,buffer + _offset,offset );
+                _offset += offset;
+
+                return *this;
+            }
+
+            inline operator_t &operator << ( int8 &val )
+            {
+                static uint32 offset = sizeof( int8 );
+                if ( _offset + offset > _size )
+                {
+                    assert( "stream::io write int8 overflow",false );
+                    return *this;
+                }
+
+
+            }
+#undef LDR
+#undef STR
+        private:
+            /* 提供一个声明的模板防止未知类型做隐式转换 */
+            template <typename T> operator_t& operator >> ( T& val );
+            template <typename T> operator_t& operator << ( T& val );
+
+            const char *_buffer;
+            const uint32 _size ;
+            uint32 _offset;
+    };
 private:
     std::map< std::pair<int32,int32>,struct protocol > _protocol;
 };
