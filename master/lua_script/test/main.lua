c3f1eff92f7794bda5753fc8cd92b2b6ad24aaba
changnet <940477049@qq.com>
2016-07-28 21:27:38 +0800

    protobuf reflection test

--- master/lua_script/test/main.lua
+++ master/lua_script/test/main.lua
@@ -22,9 +22,8 @@ local function main()
     -- require "example.mongo_performance"
     -- require "example.mysql_performance"
     -- require "example.log_performance"
-    require "example.http_performance"
-    -- require "example.stream_performance"
-    -- require "example.stream_protocol_test"
+    -- require "example.http_performance"
+    require "example.stream_performance"
     -- require "example.words_filter_performance"
 
     print( obj_counter.obj_count("Http_socket") )
