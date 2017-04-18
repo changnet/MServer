618da1c6c97cdcdfbcfcdc26b0ff37dde512b1d6
changnet <940477049@qq.com>
2017-04-18 21:46:00 +0800

    cppcheck cfg

--- shell/build_env.sh
+++ shell/build_env.sh
@@ -31,5 +31,17 @@ cd ..
 
 # TODO build flatbuffers
 
+# TODO build cppcheck
+# https://sourceforge.net/projects/cppcheck/files/cppcheck/1.78/cppcheck-1.78.tar.gz/download
+# tar -zxvf cppcheck-1.78.tar.gz
+# cd cppcheck-1.78
+# make SRCDIR=build CFGDIR=cfg HAVE_RULES=yes
+# CFGDIR表示使用当前目录下的cfg目录，如果要make install，请另外指定目录并复制当前cfg目录到指定的目录
+
+# make: pcre-config: Command not found
+# If you write HAVE_RULES=yes then pcre must be available. So you have two options:
+# 1. Remove HAVE_RULES=yes
+# 2. Install pcre(apt-get install libpcre3-dev)
+
 rm -R lua-5.3.1
 rm -R mongo-c-driver-1.2.1
--- shell/cppcheck.sh
+++ shell/cppcheck.sh
@@ -5,6 +5,8 @@
 # --enable=information
 # --enable=warning,performance
 
-
-
-cppcheck --enable=all -i../master/deps ../master
+# -D_ASSERT_H -UNDEBUG 由于重写了assert.h，需要显式指定这些宏定义
+# -D EPOLL_CLOEXEC ev.cpp中是否定义是否使用新版本epoll_create1
+# --library need cppcheck1.78
+# --check-config Cppcheck cannot find all the include files (use --check-config for details)
+/home/xzc/cppcheck-1.78/cppcheck -DTCP_USER_TIMEOUT -UEPOLL_CLOEXEC -D_ASSERT_H -UNDEBUG --library=cppcheck.cfg --enable=all -i../master/deps ../master
