c67926698f46b713ece8591f11e1d61a41950510
changnet <940477049@qq.com>
2017-04-19 21:17:13 +0800

    clang oclint update mongo-c-driver

--- shell/build_env.sh
+++ shell/build_env.sh
@@ -3,31 +3,64 @@
 # build development enviroment
 # install 3th party software or library,like lua、mongodb
 
-apt-get install gcc
-apt-get install g++
-apt-get install make
-apt-get install cmake
+PKGDIR=../package
 
-apt-get install uuid-dev
-apt-get install mysql-server
-apt-get install libmysqlclient-dev
-apt-get install libreadline6-dev
-apt-get install pkg-config libssl-dev libsasl2-dev
+# install base compile enviroment
+function build_base()
+{
+    apt-get install gcc
+    apt-get install g++
+    apt-get install make
 
-cd ../package
+    # flatbuffers mongo may need this
+    apt-get install cmake
+    apt-get install automake
+}
 
-tar -zxvf lua-5.3.1.tar.gz
-cd lua-5.3.1
-make linux install
-make install
-cd ..
+# install third party library
+function build_library()
+{
+    apt-get install uuid-dev
+    apt-get install mysql-server
+    apt-get install libmysqlclient-dev
+    apt-get install libreadline6-dev
+    apt-get install pkg-config libssl-dev libsasl2-dev
+}
 
-tar -zxvf mongo-c-driver-1.2.1.tar.gz
-cd mongo-c-driver-1.2.1
-./configure
-make
-make install
-cd ..
+function build_lua()
+{
+    cd $PKGDIR
+
+    tar -zxvf lua-5.3.1.tar.gz
+    cd lua-5.3.1
+    make linux install
+    make install
+
+    cd -
+    rm -R $PKGDIR/lua-5.3.1
+}
+
+function build_mongo_driver()
+{
+    # 如果出现aclocal-1.14: command not found,安装automake，版本不对
+    # 则修改以下两个文件的am__api_version='1.15'，然后重新./configure
+    # mongo-c-driver-1.2.1\configure
+    # mongo-c-driver-1.2.1\src\libbson\configure
+    cd $PKGDIR
+
+    tar -zxvf mongo-c-driver-1.6.2.tar.gz
+    cd mongo-c-driver-1.6.2
+    ./configure --disable-automatic-init-and-cleanup
+    make
+    make install
+
+    cd -
+    rm -R $PKGDIR/mongo-c-driver-1.6.2
+}
+
+build_library
+build_lua
+build_mongo_driver
 
 # TODO build flatbuffers
 
@@ -43,5 +76,21 @@ cd ..
 # 1. Remove HAVE_RULES=yes
 # 2. Install pcre(apt-get install libpcre3-dev)
 
-rm -R lua-5.3.1
-rm -R mongo-c-driver-1.2.1
+# TODO build oclint env
+# need debian8(>=gcc5)
+# edit /etc/apt/sources.list
+# deb-src http://mirrors.163.com/debian-security/ jessie/updates main non-free contrib
+# apt-get update
+# apt-get install g++-6
+# download https://github.com/oclint/oclint/releases
+# tar -zxvf xx.tar.gz
+# cd xxx
+# bin/oclint -version
+
+# TODO build bear(oclint tool)
+# https://github.com/rizsotto/Bear/archive/2.2.1.tar.gz bear2.2.1.tar.gz
+# tar -zxvf bear2.2.1.tar.gz
+# cd Bear-2.2.1/
+# cmake .
+# make
+# make install (一定要安装，因为要用到libbear.so)
---
+++ shell/oclint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 安装好oclint bear,参考build_env.sh
+
+BUILD_DIR=../master/bin
+SOURCE_DIR=../master
+
+cd $SOURCE_DIR
+
+# 一定要清除，不然有部分文件没变化，不会编译
+make clean
+
+# bear会把每个文件编译参数记录到 compile_commands.json，类似
+# {
+#     "command": "c++ -c -std=c++11 -pipe -Wall -g3 -pedantic -O0 -I./deps/http-parser -I./deps/lua_parson -I./deps/lua_rapidxml -I/usr/local/include/libmongoc-1.0 -I/usr/local/include/libbson-1.0 -I/usr/include/mysql -I./deps/aho-corasick -I./deps/lua_flatbuffers -I./deps/lua_bson -MFbin/global/global.d -o bin/global/global.o global/global.cpp", 
+#     "directory": "/media/sf_linux_share/github/MServer/master", 
+#     "file": "/media/sf_linux_share/github/MServer/master/global/global.cpp"
+# }, 
+bear make
+
+# oclint要求在build目录和源代码目录各保留一份
+cp compile_commands.json bin/
+
+# oclint -p bin global/global.cpp lua_cpplib/lsocket.cpp ...
+make oclint
