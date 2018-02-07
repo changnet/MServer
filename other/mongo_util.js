f79c5803bf354b1d6837cf8b8f9de78295273bb8
changnet <940477049@qq.com>
2018-02-07 15:52:46 +0800

    add some mongodb shell

--- other/mongo_util.js
+++ other/mongo_util.js
@@ -1,11 +1,7 @@
-function new_admin()
-{
-    show databases;
-}
-
+// mongo 127.0.0.1:27013/db_test mongo_util.js 可以加载js文件
+// 注意js文件中不能运行use admin等mongo shell命令。只能写标准js
 function new_db()
 {
-    show databases;
 }
 
 new_db();
--- shell/mongo.sh
+++ shell/mongo.sh
@@ -4,6 +4,24 @@
 
 # 写这个脚本是因为我开发的环境MongoDB并不作为service来运行的
 
+# mongo 127.0.0.1:27013
+# mongo 127.0.0.1:27013/mudrv -u test -p test
+# use admin
+# db.createUser( {user:"xzc",pwd:"1",roles:["userAdminAnyDatabase","dbAdminAnyDatabase"]} )
+# use mudrv
+# db.createUser( {user:"test",pwd:"test",roles:["dbOwner","userAdmin","readWrite"]} )
+# show users;
+# show dbs;
+# show collections;
+# db.item.find();
+
+# mongo可以运行js文件，但是这个js文件是不能用"use admin"这个mongo shell命令的
+# mongo 127.0.0.1:27013/mudrv -u test -p test command.js
+
+# 还可以把mongo shell命令写在文件中，直接导入mongo，这个和使用js文件是不一样的
+# mongo 127.0.0.1:27013/mudrv -u test -p test < command.js
+
+
 DBPATH=/home/xzc/mongodb
 MONGOD=$DBPATH/bin/mongod
 MONGO=$DBPATH/bin/mongo
@@ -18,16 +36,16 @@ function stop()
     $MONGOD --shutdown -f $DBPATH/mongod.conf
 }
 
-# ./mongo.sh db_shell test test test_999
-function db_shell()
+# ./mongo.sh shell 用户名 密码 数据库
+# ./mongo.sh shell test test test_999
+function shell()
 {
-    echo $1 $2 $3
-    $MONGOD -u$1 -p$2 --port 27013 $3
+    $MONGO -u$1 -p$2 --port 27013 $3
 }
 
 function new_db()
 {
-    $MONGO --nodb --norc ../other/mongo_util.js $1
+    $MONGO -u$1 -p$2 --port 27013 admin < ../other/mongo_new.ms
 }
 
 parameter=($@)
