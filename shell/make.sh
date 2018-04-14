dd7b940510d01690ef3a39da5df8fd55406825a2
changnet <940477049@qq.com>
2018-04-14 14:44:58 +0800

    move cpp source files into cpp_src dir

--- shell/make.sh
+++ shell/make.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-time ../other/colormake.sh --makefile=../other/Makefile -C ../master $1
+# --makefile中的路径是相对于源码路径的
+time ../other/colormake.sh --makefile=../../other/Makefile -C ../master/cpp_src $1
