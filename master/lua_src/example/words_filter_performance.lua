82c4a191b2132a9cd3a4bd97261019e8b2e883e9
changnet <940477049@qq.com>
2018-04-30 19:07:40 +0800

    adjust framework directory

--- master/lua_src/example/words_filter_performance.lua
+++ master/lua_src/example/words_filter_performance.lua
@@ -2,7 +2,7 @@ local Acism = require "Acism"
 
 local acism = Acism()
 
-acism:load_from_file( "lua_src/config/illeagal_worlds.txt",false )
+acism:load_from_file( "config/illeagal_worlds.txt",false )
 
 -- scan返回匹配字符串尾位置，一般一个中文字符占3
 print( "acism worlds filter",acism:scan("this is nothing") )
