e2fd47486a10e770d6fc906c605ed3f037ba7ba2
changnet <940477049@qq.com>
2018-10-17 21:28:13 +0800

    prepare for aoi

--- master/cpp_src/scene/grid_aoi.h
+++ master/cpp_src/scene/grid_aoi.h
@@ -12,26 +12,55 @@ class grid_aoi
 {
 public:
     typedef entity_id_t uint32; // 用来标识实体的唯一id
+    typedef std::vector< entity_id_t > grid_entity_t; // 单个格子中的实体列表
+
     struct entity_ctx
     {
-        uint8 _mask;
-        int32 _pos_x;
-        int32 _pos_y;
+        uint8 _type; // 记录实体类型
+        uint8 _pos_x; // 格子坐标，x
+        uint8 _pos_y; // 格子坐标，y
         entity_id_t _id;
         // 关注我的实体列表。比如我周围的玩家，需要看到我移动、放技能
-        // 都需要频繁广播给他们。如果游戏并不是arpg，那么可能并不这个列表
-        std::vector< entity_id_t > *_watch_me;
+        // 都需要频繁广播给他们。如果游戏并不是arpg，可能并不这个列表
+        std::vector< entity_id_t > _watch_me;
     };
 public:
     grid_aoi();
     virtual ~grid_aoi();
 
-    bool exit_entity(entity_id_t id);
-    bool enter_entity(entity_id_t id,int32 x,int32 y,uint8 mask);
-    bool update_entity(entity_id_t id,int32 x,int32 y,uint8 mask);
-private:
-    map_t< entity_id_t,struct entity_ctx > _entity_set;
+    void set_watch_mask(uint32 mask); // 设置需要放入watch_me列表的实体类型
+    void set_visual_range(int32 width,int32 height); // 设置视野
+    void set_size(int32 width,int32 height,int32 pix); // 设置宽高，格子像素
 
+    /* 获取某一范围内实体
+     * 底层这里只支持矩形，如果是其他形状的，上层根据实体位置再筛选即可
+     */
+    int32 get_entitys(entity_id_t *list,
+        int32 max,uint32 mask,int32 srcx,int32 srcy,int32 destx,int32 desty);
+
+    bool exit_entity(entity_id_t id);
+    bool enter_entity(entity_id_t id,int32 x,int32 y,uint8 type);
+    bool update_entity(entity_id_t id,int32 x,int32 y,uint8 type);
+private:
+    /* 需要放入watch_me列表的实体类型，按位取值
+     * 一般来说只有玩家需要放进去，因为实体移动、攻击等需要广播给这些玩家
+     * 一些游戏或者手游全屏广播的场景，不需要开启这个列表
+     */
+    uint32 _watch_mask;
+
+    uint8 _width; // 场景最大宽度(格子坐标)
+    uint8 _height; // 场景最大高度(格子坐标)
+    uint32 _grid_pix; // 多少像素转换为一个格子边长
+    uint8 _visual_width; // 视野宽度格子数
+    uint8 _visual_height; // 视野高度格子数
+
+    /* 记录每个格子中的实体id列表
+     * 格子的x、y坐标为uint16，一起拼成一个uint32作为key
+     */
+    map_t< uint32,grid_entity_t > _entity_grid;
+
+    /* 记录所有实体的数据 */
+    map_t< entity_id_t,struct entity_ctx > _entity_set;
 };
 
 #endif /* __GRID_AOI_H__ */
