82ef93e4fd3d407642252ea593cb764c10959c2b
changnet <940477049@qq.com>
2018-10-23 18:24:40 +0800

    aoi

---
+++ master/cpp_src/lua_cpplib/laoi.h
@@ -0,0 +1,28 @@
+#ifndef __LAOI_H__
+#define __LAOI_H__
+
+#include <lua.hpp>
+#include "../scene/grid_aoi.h"
+
+class laoi : public grid_aoi
+{
+public:
+    ~laoi();
+    explicit laoi( lua_State *L );
+
+    int32 set_watch_mask( lua_State *L ); // 设置需要放入watch_me列表的实体类型
+    int32 set_visual_range( lua_State *L ); // 设置视野
+    int32 set_size( lua_State *L ); // 设置宽高，格子像素
+
+    int32 get_all_entitys(lua_State *L);
+    /* 获取某一范围内实体
+     * 底层这里只支持矩形，如果是其他形状的，上层根据实体位置再筛选即可
+     */
+    int32 get_entitys( lua_State *L );
+
+    int32 exit_entity( lua_State *L );
+    int32 enter_entity( lua_State *L );
+    int32 update_entity( lua_State *L );
+};
+
+#endif /* __LAOI_H__ */
