9cd4752ebc967df5d6fefa0e8a8bd3a8203d8cf4
changnet <940477049@qq.com>
2018-11-02 17:36:15 +0800

    rank

--- master/cpp_src/util/rank.cpp
+++ master/cpp_src/util/rank.cpp
@@ -2,7 +2,28 @@
 
 base_rank::base_rank()
 {
-    _max_count = 0; // 排行榜最大数量
+    _max_count = 64; // 排行榜最大数量(默认64)
     _rank_count = 0; // 当前排行榜中数量
     _max_factor = 0; // 当前排行榜使用到的最大排序因子数量
 }
+
+// =========================== insertion rank ================================
+insertion_rank::insertion_rank()
+{
+    _objects = NULL;
+}
+
+insertion_rank::~insertion_rank()
+{
+    delete []_objects;
+    _objects = NULL;
+}
+
+int32 insertion_rank::remove(int32 id);
+int32 insertion_rank::insert(int32 id,factor_t factor);
+int32 insertion_rank::update(int32 id,int64 factor,int32 factor_idx);
+
+int32 insertion_rank::get_rank_by_id(int32 id);
+factor_t &insertion_rank::get_factor(int32 id);
+int32 insertion_rank::get_id_by_rank(int32 rank);
+int32 insertion_rank::get_top(int64 *list,int32 n);
--- master/cpp_src/util/rank.h
+++ master/cpp_src/util/rank.h
@@ -14,23 +14,62 @@
 class base_rank
 {
 public:
-    typedef int64[MAX_RANK_FACTOR] factor_t;
+    typedef int64 factor_t[MAX_RANK_FACTOR];
+    class object_t
+    {
+    public:
+        object_t(){};
+        ~object_t(){};
+        object_t(const object_t &obj)
+        {
+            _id = obj._id;
+            for (int32 idx = 0;idx < MAX_RANK_FACTOR;idx ++)
+            {
+                _factor[idx] = obj._factor[idx];
+            }
+        }
+
+        // 多因子对比
+        inline int32 compre(
+            const object_t *src,const object_t *dest,int32 max_idx)
+        {
+            for (int32 idx = 0;idx < max_idx;idx ++)
+            {
+                if (src->_factor[idx] == dest->_factor[idx]) continue;
+
+                return src->_factor[idx] < dest->_factor[idx] ? -1 : 1;
+            }
+
+            return 0;
+        }
+    public:
+        int32 _id;
+        factor_t _factor;
+    };
 public:
     base_rank();
     virtual ~base_rank();
 
+    // 删除一个对象
     virtual int32 remove(int32 id) = 0;
+    // 插入一个对象
     virtual int32 insert(int32 id,factor_t factor) = 0;
-    virtual int32 update(int32 id,int64 factor,int32 factor_idx) = 0;
+    // 更新一个对象排序因子
+    virtual int32 update(int32 id,int64 factor,int32 factor_idx = 0) = 0;
 
     virtual int32 get_rank_by_id(int32 id) = 0;
     virtual factor_t &get_factor(int32 id) = 0;
     virtual int32 get_id_by_rank(int32 rank) = 0;
     virtual int32 get_top(int64 *list,int32 n) = 0;
 
+    // 清除排行的对象，但不清除内存
+    void clear() { _rank_count = 0; }
+    // 设置排行榜上限，超过此上将不会进入排行榜
     void set_max_count(int32 max) { _max_count = max; };
 
+    // 获取当前排行榜用到的排序因子数量
     int32 get_max_factor() const { return _max_factor; };
+    // 获取当前排行榜中对象数量
     int32 get_rank_count() const { return _rank_count; };
 protected:
     int32 _max_count; // 排行榜最大数量
@@ -38,9 +77,24 @@ protected:
     uint8 _max_factor; // 当前排行榜使用到的最大排序因子数量
 };
 
+// 插入法排序
 class insertion_rank : public base_rank
 {
 public:
+    insertion_rank();
+    ~insertion_rank();
+
+    int32 remove(int32 id);
+    int32 insert(int32 id,factor_t factor);
+    int32 update(int32 id,int64 factor,int32 factor_idx = 0);
+
+    int32 get_rank_by_id(int32 id);
+    factor_t &get_factor(int32 id);
+    int32 get_id_by_rank(int32 rank);
+    int32 get_top(int64 *list,int32 n);
+private:
+    object_t **_object_list;
+    map_t< int32,object_t* > _object_set;
 };
 
 #endif /* __RANK_H__ */
