5da70458d1670478fc4cccd0169293dbfe494882
changnet <940477049@qq.com>
2019-04-15 20:29:58 +0800

    const char map

---
+++ master/cpp_src/global/map.h
@@ -0,0 +1,129 @@
+#ifndef __MAP_H__
+#define __MAP_H__
+
+
+//--------------------------------------------------------------------------
+// MurmurHash2, by Austin Appleby
+// Note - This code makes a few assumptions about how your machine behaves -
+
+// 1. We can read a 4-byte value from any address without crashing
+// 2. sizeof(int) == 4
+
+// And it has a few limitations -
+
+// 1. It will not work incrementally.
+// 2. It will not produce the same results on little-endian and big-endian
+//    machines.
+
+static inline
+unsigned int MurmurHash2 ( const void * key, int len, unsigned int seed )
+{
+    // 'm' and 'r' are mixing constants generated offline.
+    // They're not really 'magic', they just happen to work well.
+
+    const unsigned int m = 0x5bd1e995;
+    const int r = 24;
+
+    // Initialize the hash to a 'random' value
+
+    unsigned int h = seed ^ len;
+
+    // Mix 4 bytes at a time into the hash
+
+    const unsigned char * data = (const unsigned char *)key;
+
+    while(len >= 4)
+    {
+        unsigned int k = *(unsigned int *)data;
+
+        k *= m;
+        k ^= k >> r;
+        k *= m;
+
+        h *= m;
+        h ^= k;
+
+        data += 4;
+        len -= 4;
+    }
+
+    // Handle the last few bytes of the input array
+
+    switch(len)
+    {
+    case 3: h ^= data[2] << 16;
+    case 2: h ^= data[1] << 8;
+    case 1: h ^= data[0];
+            h *= m;
+    };
+
+    // Do a few final mixes of the hash to ensure the last few
+    // bytes are well-incorporated.
+
+    h ^= h >> 13;
+    h *= m;
+    h ^= h >> 15;
+
+    return h;
+}
+
+struct c_string
+{
+    size_t _length;
+    unsigned int _hash;
+    const char *_raw_ctx;
+    c_string(const char *ctx)
+    {
+        _raw_ctx = ctx;
+        _length = strlen(ctx);
+        _hash = MurmurHash2(ctx,_length,static_cast<size_t>(0xc70f6907UL));
+    }
+};
+
+/* 自定义类型hash也可以放到std::hash中，暂时不这样做
+ * https://en.cppreference.com/w/cpp/utility/hash
+ */
+
+/* the default hash function in libstdc++:MurmurHashUnaligned2
+ * https://sites.google.com/site/murmurhash/ by Austin Appleby
+ * other hash function(djb2,sdbm) http://www.cse.yorku.ca/~oz/hash.html */
+struct hash_c_string
+{
+    size_t operator()(const c_string &cs) const
+    {
+        return cs._hash;
+    }
+};
+
+/* compare function for const char* */
+struct cmp_const_char
+{
+    bool operator()(const char *a, const char *b) const
+    {
+        return std::strcmp(a, b) < 0;
+    }
+};
+
+/* compare function for const char* */
+struct equal_c_string
+{
+    bool operator()(const c_string &a, const c_string &b) const
+    {
+        return 0 == std::strcmp(a._raw_ctx, b._raw_ctx);
+    }
+};
+
+/* 需要使用hash map，但又希望能兼容旧版本时使用map_t */
+#if __cplusplus < 201103L    /* -std=gnu99 */
+    #include <map>
+    #define map_t    std::map
+    #define const_char_map_t(T) std::map<const char *,T,cmp_const_char>
+#else    /* if support C++ 2011 */
+    #include <unordered_map>
+    #define map_t    std::unordered_map
+    // TODO:template<class T> using const_char_map_t = ...，但03版本不支持
+    #define const_char_map_t(T)    \
+        std::unordered_map<const c_string,T,hash_c_string,equal_c_string>
+#endif
+
+#endif /* __MAP_H__ */
--- test/unordered_map.cpp
+++ test/unordered_map.cpp
@@ -1,5 +1,13 @@
 #include <map>
 #include <ctime>
+#include <cstdio>
+#include <vector>
+#include <cstdlib>     /* srand, rand */
+
+#include <cstring>
+#include <iostream>
+
+#include "../master/cpp_src/global/types.h"
 
 #if(__cplusplus >= 201103L)
 # include <unordered_map>
@@ -56,7 +64,8 @@ void run_unordered_map_test()
             _protocol.insert( std::make_pair(std::make_pair(m,n),result) );
         }
 
-    std::cout << "unordered_map create cost " << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
+    std::cout << "unordered_map create cost "
+        << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
 
     start = clock();
     for ( unsigned short m = 0;m < M_TS;m ++ )
@@ -69,7 +78,8 @@ void run_unordered_map_test()
                 return;
             }
         }
-    std::cout << "unordered_map find cost " << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
+    std::cout << "unordered_map find cost "
+        << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
 }
 
 void run_std_map_test()
@@ -84,7 +94,8 @@ void run_std_map_test()
             _protocol.insert( std::make_pair(std::make_pair(m,n),result) );
         }
 
-    std::cout << "std_map create cost " << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
+    std::cout << "std_map create cost "
+        << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
 
     start = clock();
     for ( unsigned short m = 0;m < M_TS;m ++ )
@@ -97,19 +108,178 @@ void run_std_map_test()
                 return;
             }
         }
-    std::cout << "std_map find cost " << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
+    std::cout << "std_map find cost "
+        << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
 }
 
+void create_random_key(std::vector<std::string> &vt)
+{
+    srand (time(NULL));
+
+    for (int idx = 0;idx < 100000;idx ++)
+    {
+        int ikey = rand();
+        int ikey2 = rand();
+
+        char skey[64];
+        sprintf(skey,"%X%X",ikey,ikey2);
+
+        vt.push_back(skey);
+    }
+}
+
+void test_unorder_string(const std::vector<std::string> &vt)
+{
+    std::unordered_map<std::string,int> test_map;
+
+    clock_t start = clock();
+    for (int idx = 0;idx < vt.size();idx ++)
+    {
+        test_map[ vt[idx].c_str() ] = idx;
+    }
+    std::cout << "unorder_map std::string create cost "
+        << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
+
+    start = clock();
+    for (int idx = 0;idx < vt.size()/2;idx ++)
+    {
+        if (test_map.find(vt[idx].c_str()) == test_map.end())
+        {
+            std::cout << "unorder_map std::string find fail" << std::endl;
+            return;
+        }
+    }
+
+    std::cout << "unorder_map std::string find cost "
+        << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
+}
+
+
+void test_unorder_char(const std::vector<std::string> &vt)
+{
+    std::unordered_map<const c_string,int,hash_c_string,equal_c_string> test_map;
+
+    clock_t start = clock();
+    for (int idx = 0;idx < vt.size();idx ++)
+    {
+        test_map[ vt[idx].c_str() ] = idx;
+    }
+    std::cout << "unorder_map char create cost "
+        << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
+
+    start = clock();
+    for (int idx = 0;idx < vt.size()/2;idx ++)
+    {
+        if (test_map.find(vt[idx].c_str()) == test_map.end())
+        {
+            std::cout << "unorder_map char find fail" << std::endl;
+            return;
+        }
+    }
+
+    std::cout << "unorder_map char find cost "
+        << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
+}
+
+void test_stdmap_char(const std::vector<std::string> &vt)
+{
+    std::map<const char *,int,cmp_const_char> test_map;
+
+    clock_t start = clock();
+    for (int idx = 0;idx < vt.size();idx ++)
+    {
+        test_map[ vt[idx].c_str() ] = idx;
+    }
+    std::cout << "std_map char create cost "
+        << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
+
+    start = clock();
+    for (int idx = 0;idx < vt.size()/2;idx ++)
+    {
+        if (test_map.find(vt[idx].c_str()) == test_map.end())
+        {
+            std::cout << "std_map char find fail" << std::endl;
+            return;
+        }
+    }
+
+    std::cout << "std_map char find cost "
+        << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
+}
+
+
+void test_stdmap_string(const std::vector<std::string> &vt)
+{
+    std::map<std::string,int> test_map;
+
+    clock_t start = clock();
+    for (int idx = 0;idx < vt.size();idx ++)
+    {
+        test_map[ vt[idx] ] = idx;
+    }
+    std::cout << "std_map string create cost "
+        << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
+
+    start = clock();
+    for (int idx = 0;idx < vt.size()/2;idx ++)
+    {
+        if (test_map.find(vt[idx]) == test_map.end())
+        {
+            std::cout << "std_map char find fail" << std::endl;
+            return;
+        }
+    }
+
+    std::cout << "std_map string find cost "
+        << (float(clock()-start))/CLOCKS_PER_SEC << std::endl;
+}
+
+
 /*
-unordered_map create cost 0.413932
-unordered_map find cost 0.129531
-std_map create cost 1.60213
-std_map find cost 0.915265
+
+unordered_map create cost 0.08
+unordered_map find cost 0.01
+std_map create cost 0.28
+std_map find cost 0.13
+
+key为10000000时：
+std_map char create cost 31.73
+std_map char find cost 15.69
+std_map string create cost 56.44
+std_map string find cost 28.48
+unorder_map char create cost 11.61
+unorder_map char find cost 1.17
+unorder_map std::string create cost 11.75
+unorder_map std::string find cost 2.13
+
+key为100000时
+std_map char create cost 0.09
+std_map char find cost 0.04
+std_map string create cost 0.15
+std_map string find cost 0.08
+unorder_map char create cost 0.03
+unorder_map char find cost 0.01
+unorder_map std::string create cost 0.06
+unorder_map std::string find cost 0.02
+
 */
 
+// valgrind --tool=callgrind --instr-atstart=no ./unoder_map
 int main()
 {
     run_unordered_map_test();
     run_std_map_test();
+
+    std::vector<std::string> vt;
+
+    create_random_key(vt);
+
+    test_stdmap_char(vt);
+    test_stdmap_string(vt);
+    int idx = 0; // wait valgrind:callgrind_control -i on
+    //std::cin >> idx;
+    test_unorder_char(vt);
+    test_unorder_string(vt);
+    //std::cin >> idx;
     return 0;
 }
