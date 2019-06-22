22880006bf8d7cca7acd2b2873fb6ecd7e8aac57
changnet <940477049@qq.com>
2019-06-22 17:56:33 +0800

    fix exit crash after local static variable destory

--- master/cpp_src/system/static_global.cpp
+++ master/cpp_src/system/static_global.cpp
@@ -4,24 +4,17 @@
 #include "../mysql/sql.h"
 #include "../mongo/mongo.h"
 
-/* 各个变量之间有依赖，要注意顺序
- * 同一个unit中的static对象，先声明的先初始化。销毁的时候则反过来
- * 在头文件中的顺序不重要，这里实现的顺序才是运行时的顺序
- */
+class lev          *static_global::_ev          = NULL;
+class lstate       *static_global::_state       = NULL;
+class ssl_mgr      *static_global::_ssl_mgr     = NULL;
+class codec_mgr    *static_global::_codec_mgr   = NULL;
+class statistic    *static_global::_statistic   = NULL;
+class thread_log   *static_global::_async_log   = NULL;
+class thread_mgr   *static_global::_thread_mgr  = NULL;
+class lnetwork_mgr *static_global::_network_mgr = NULL;
 
-// 1. initializer最高等级初始化，在main函数之前，适合设置一些全局锁等
+// initializer最高等级初始化，在main函数之前，适合设置一些全局锁等
 class static_global::initializer static_global::_initializer;
-// 2. 状态统计，独立的
-class statistic static_global::_statistic;
-// 3. 事件主循环
-class lev static_global::_ev;
-// 4. 线程管理
-class thread_mgr static_global::_thread_mgr;
-class thread_log static_global::_async_log;
-class lstate static_global::_state;
-class codec_mgr static_global::_codec_mgr;
-class ssl_mgr static_global::_ssl_mgr;
-class lnetwork_mgr static_global::_network_mgr;
 
 int32 ssl_init();
 int32 ssl_uninit();
@@ -58,14 +51,33 @@ static_global::initializer::~initializer()
 // 业务都放这里逻辑初始化
 void static_global::initialize()  /* 程序运行时初始化 */
 {
-    _async_log.start( 1,0 );
+    /* 原本用static对象，但实在是无法控制各个销毁的顺序。因为其他文件中还有局部static对象
+     * 会依赖这些对象。
+     * 比如class lev销毁时会释放引起一些对象gc，调用局部内存池，但早就销毁了
+     * 各个变量之间有依赖，要注意顺序
+     * 同一个unit中的static对象，先声明的先初始化。销毁的时候则反过来
+     * 在头文件中的顺序不重要，这里实现的顺序才是运行时的顺序
+     */
+
+
+    // 状态统计，独立的
+    _statistic = new class statistic();
+    _ev = new class lev();
+    _thread_mgr = new class thread_mgr();
+    _async_log = new class thread_log();
+    _state = new class lstate();
+    _codec_mgr = new class codec_mgr();
+    _ssl_mgr = new class ssl_mgr();
+    _network_mgr = new class lnetwork_mgr();
+
+    _async_log->start( 1,0 );
 
     // 关服的时候，不需要等待这个线程。之前有人在关服定时器上打异步日志，导致这个线程一直忙
     // 关不了服。stop的时候会处理所有日志
-    _async_log.set_wait_busy( false );
+    _async_log->set_wait_busy( false );
 
     // 初始化流量统计，这个和时间有关，等 ev 初始化后才调用
-    _statistic.reset_trafic();
+    _statistic->reset_trafic();
 }
 
 /* 业务逻辑都放这里销毁，不能放initializer的析构函数或者等到对应static对象析构
@@ -75,9 +87,18 @@ void static_global::initialize()  /* 程序运行时初始化 */
  */
 void static_global::uninitialize() /* 程序结束时反初始化 */
 {
-    _async_log.stop();
-    _thread_mgr.stop();
-    _network_mgr.clear();
+    _async_log->stop();
+    _thread_mgr->stop();
+    _network_mgr->clear();
+
+    delete _network_mgr;
+    delete _ssl_mgr;
+    delete _codec_mgr;
+    delete _state;
+    delete _async_log;
+    delete _thread_mgr;
+    delete _ev;
+    delete _statistic;
 }
 
 
--- master/cpp_src/system/static_global.h
+++ master/cpp_src/system/static_global.h
@@ -17,7 +17,7 @@
  *
  * initializer只放业务无关的初始化，比如全局锁、ssl初始化...
  * 业务逻辑的初始化放initialize函数
- * 业务逻辑的销毁放uninitialize函数。业务逻辑必须在这晨销毁，不能等到static对象析构 !!!
+ * 业务逻辑的销毁放uninitialize函数。业务逻辑必须在这里销毁，不能等到static对象析构 !!!
  * 因为其他局部变量在main函数之后销毁了
  */
 class static_global
@@ -26,32 +26,33 @@ public:
     static void initialize();  /* 程序运行时初始化 */
     static void uninitialize(); /* 程序结束时反初始化 */
 
-    static class ev *ev() { return &_ev; }
-    static class lev *lua_ev() { return &_ev; }
-    static lua_State *state() { return _state.state(); }
-    static class ssl_mgr *ssl_mgr() { return &_ssl_mgr; }
-    static class codec_mgr *codec_mgr() { return &_codec_mgr; }
-    static class statistic *statistic() { return &_statistic; }
-    static class thread_log *async_log() { return &_async_log; }
-    static class thread_mgr *thread_mgr() { return &_thread_mgr; }
-    static class lnetwork_mgr *network_mgr() { return &_network_mgr; }
+    static class ev *ev() { return _ev; }
+    static class lev *lua_ev() { return _ev; }
+    static lua_State *state() { return _state->state(); }
+    static class ssl_mgr *ssl_mgr() { return _ssl_mgr; }
+    static class codec_mgr *codec_mgr() { return _codec_mgr; }
+    static class statistic *statistic() { return _statistic; }
+    static class thread_log *async_log() { return _async_log; }
+    static class thread_mgr *thread_mgr() { return _thread_mgr; }
+    static class lnetwork_mgr *network_mgr() { return _network_mgr; }
 private:
-    class initializer
+    class initializer // 提供一个等级极高的初始化
     {
     public:
         ~initializer();
         explicit initializer();
     };
 private:
-    static class initializer _initializer;
-    static class statistic _statistic;
-    static class lev _ev;
-    static class lstate _state;
-    static class thread_mgr _thread_mgr;
-    static class thread_log _async_log;
-    static class codec_mgr _codec_mgr;
-    static class ssl_mgr _ssl_mgr;
-    static class lnetwork_mgr _network_mgr;
+    static class lev          *_ev;
+    static class lstate       *_state;
+    static class ssl_mgr      *_ssl_mgr;
+    static class codec_mgr    *_codec_mgr;
+    static class statistic    *_statistic;
+    static class thread_log   *_async_log;
+    static class thread_mgr   *_thread_mgr;
+    static class lnetwork_mgr *_network_mgr;
+
+    static class initializer  _initializer;
 };
 
 #endif /* __STATIC_GLOBAL_H__ */
