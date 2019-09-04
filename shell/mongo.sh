d377e3d02c11e2209938e1eb71c7cfda507bd1ef
changnet <940477049@qq.com>
2019-09-04 22:15:42 +0800

    update mongodb mysql shell

--- shell/mongo.sh
+++ shell/mongo.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # mongo control shell
-
-# 写这个脚本是因为我开发的环境MongoDB并不作为service来运行的
+# 一些常用的mongo操作放这里。线上部署不要用这里的用户名和密码
 
 # mongo 127.0.0.1:27013
 # mongo 127.0.0.1:27013/mudrv -u test -p test
@@ -26,36 +25,70 @@
 
 # mongodb配置存放目录，默认目录在/etc/mongodb.conf
 DBPATH=/home/xzc/mongodb
-MONGOD=mongod
-MONGO=mongo
+
+# mongo 127.0.0.1:27013/admin是对的
+# mongo 127.0.0.1:27013 admin是错的，要在后面加数据库名，需要用--host和--port
+MONGO="mongo --host 127.0.0.1 --port 27013"
+TEST_MONGO="$MONGO -utest -ptest"
+ADMIN_MONGO="$MONGO -uxzc -p1"
 
 function start()
 {
-    $MONGOD -f $DBPATH/mongod.conf
+    mongod -f $DBPATH/mongod.conf
 }
 
 function stop()
 {
-    $MONGOD --shutdown -f $DBPATH/mongod.conf
+    mongod --shutdown -f $DBPATH/mongod.conf
 }
 
-# ./mongo.sh shell 用户名 密码 数据库
-# ./mongo.sh shell test test test_999
+# 创建数据库帐号管理员
+# mongodb和MySQL不一样，每个数据库都必须创建专属的帐号。而创建创建帐号需要帐号管理员
+# 帐号管理员只能管理帐号，不能管理数据库数据
+# 这是数据库的第一个管理员，所以要去/etc/mongod.conf注释掉authorization选项
+# systemctl start mongod
+# 运行完脚本后，再加上authorization然后重启数据库
+function admin()
+{
+# Read：允许用户读取指定数据库
+# readWrite：允许用户读写指定数据库
+# dbAdmin：允许用户在指定数据库中执行管理函数，如索引创建、删除，查看统计或访问system.profile
+# userAdmin：允许用户向system.users集合写入，可以找指定数据库里创建、删除和管理用户
+# dbOwner:包括readWrite、dbAdmin、userAdmin的权限
+# clusterAdmin：只在admin数据库中可用，赋予用户所有分片和复制集相关函数的管理权限。
+# readAnyDatabase：只在admin数据库中可用，赋予用户所有数据库的读权限
+# readWriteAnyDatabase：只在admin数据库中可用，赋予用户所有数据库的读写权限
+# userAdminAnyDatabase：只在admin数据库中可用，赋予用户所有数据库的userAdmin权限
+# dbAdminAnyDatabase：只在admin数据库中可用，赋予用户所有数据库的dbAdmin权限。
+# root：只在admin数据库中可用。超级账号，超级权限
+    $MONGO << EOF
+use admin
+db.createUser( {user:"xzc",pwd:"1",roles:["userAdminAnyDatabase","dbAdminAnyDatabase"]} )
+EOF
+}
+
+# ./mongo.sh shell 数据库
+# 进入测试服数据库shell ./mongo.sh shell test_999
 function shell()
 {
-    $MONGO -u$1 -p$2 --port 27013 $3
+    $TEST_MONGO $1
 }
 
-# ./mongo.sh new_db xzc 1
+# 创建测试用的数据库test_999
+# ./mongo.sh new_db
 function new_db()
 {
-    $MONGO -u$1 -p$2 --port 27013 admin < ../other/mongo_new.ms
+    $ADMIN_MONGO admin << EOF
+use test_999
+db.createUser( {user:"test",pwd:"test",roles:["dbAdmin","readWrite"]} )
+EOF
 }
 
-# ./mongo.sh cmd test test test_999 mongo_clear
+# 运行mongo脚本文件
+# ./mongo.sh cmd test_999 mongo_clear
 function cmd()
 {
-    $MONGO -u$1 -p$2 --port 27013 $3 < ../other/$4.ms
+    $TEST_MONGO $3 < ../other/$4.ms
 }
 
 parameter=($@)
--- shell/mysql.sh
+++ shell/mysql.sh
@@ -2,12 +2,24 @@
 
 # mysql control shell
 
+# 查看使用哪个配置文件 mariadb --verbose --help | grep my.cnf，一般是/etc/mysql/my.cnf
+
+# mariadb的配置在 cat /etc/mysql/mariadb.conf.d/50-server.cnf
+# debian系统下，/etc/mysql/debian.cnf配置root用户不用密码进入
+
+# 去掉 skip-grant-tables
+# 设置root密码: SET PASSWORD FOR 'root'@'localhost' = PASSWORD('1');
+# select user,host,password from mysql.user; 保证没有匿名用户，无密码用户
+# mysql_secure_installation这个指令可以做一些安全设置
+
+# systemctl restart mariadb
+
 # 创建内网测试用户，正式服不要给这么高的权限并且要限制登录ip
 # ./mysql.sh new_user 管理员用户名 管理员密码 新用户名 新用户密码
 # ./mysql.sh new_user root 1 test test
 function new_user()
 {
-    mysql -u$1 -p$2 <<EOF 
+    mysql -u$1 -p$2 <<EOF
     CREATE USER "$3"@'%' IDENTIFIED BY "$4";
     GRANT ALL PRIVILEGES ON *.* TO "$3"@'%' WITH GRANT OPTION;
     flush privileges;
@@ -19,7 +31,7 @@ EOF
 # ./mysql.sh new_log_schema test test test_999
 function new_log_schema()
 {
-    mysql -u$1 -p$2 <<EOF 
+    mysql -u$1 -p$2 <<EOF
     create schema $3 default character set utf8 collate utf8_general_ci;
 EOF
 
