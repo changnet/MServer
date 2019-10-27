6511e2c8e9bea2b638d6698be3de42a399067d6b
changnet <940477049@qq.com>
2019-10-27 12:09:14 +0800

    add release option for makefile

--- shell/make.sh
+++ shell/make.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 # --makefile中的路径是相对于源码路径的
-time ../project/colormake.sh --makefile=../../project/Makefile -C ../master/cpp_src $1
+
+SRC=../master/cpp_src
+MAKEFILE=../../project/Makefile
+
+target=$1
+
+# ./make.sh release 编译发布版本
+
+if [ "$target" == "release" ]; then
+    echo release now $CMD
+    time ../project/colormake.sh --makefile=$MAKEFILE -C $SRC BUILD_OPT=release
+else
+    time ../project/colormake.sh --makefile=$MAKEFILE -C $SRC $target
+fi
