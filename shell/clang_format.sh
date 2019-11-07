330b234814299524f85ed6be8f339ce38ad89e5d
changnet <940477049@qq.com>
2019-11-07 21:29:24 +0800

    update clang-format

--- shell/clang_format.sh
+++ shell/clang_format.sh
@@ -19,6 +19,12 @@ while IFS=  read -r -d $'\0'; do
     FILES+=("$REPLY")
 done < <(sh -c "find $SRC -regex '.*\.\(cpp\|hpp\|cc\|cxx\|h\)' $EX_PATH -print0")
 
+# clang-foramt不支持直接传入配置路径，但是把.clang-format和源代码混合也不太好
+# 1. 尝试使用cat 文件到clang-format，再使用-assume-filename参数是可以指定配置路径
+#    但无法重定向输出到同一文件，因为重定向不允许这样操作
+
+# 现在是把配置复制到源代码根目录，用完再删除
+
 cp $CF_FILE $SRC
 
 for F in ${FILES[@]}; do
