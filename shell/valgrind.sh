66ae6314b819418430f34ca84c89430d40d1ffc2
changnet <940477049@qq.com>
2020-05-25 19:33:04 +0800

    update valgrind shell

--- shell/valgrind.sh
+++ shell/valgrind.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-cd ../master/bin
+cd ../server/bin
 
 # ./valgrind.sh mem test 1 1
 if [ "$1" == "mem" ]; then
     valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all --suppressions=../project/valgrind.suppressions --gen-suppressions=all master $2 1 1
 elif [ "$1" == "call" ]; then
     valgrind --tool=callgrind ./master $2 1 1
+elif [ "$1" == "massif" ]; then
+    # ms_print
+    # 配合gdb malloc_stats malloc_info(0, stdout)
+    valgrind --tool=massif ./master $2 1 1
 fi
