e65b6113df95ecb93e6fd292645380e012052211
changnet <940477049@qq.com>
2020-07-11 12:19:10 +0800

    auto generate protocol

--- shell/auto_proto.sh
+++ shell/auto_proto.sh
@@ -10,3 +10,5 @@ WORK_DIR=`dirname $DIR`/server/proto
 # 自动生成协议
 # lua $WORK_DIR/auto.lua $WORK_DIR/cs.lua $WORK_DIR/auto_cs.lua ts $WORK_DIR/auto_cs.ts
 lua $WORK_DIR/auto.lua $WORK_DIR/cs.lua $WORK_DIR/auto_cs.lua
+
+lua $WORK_DIR/auto.lua $WORK_DIR/ss.lua $WORK_DIR/auto_ss.lua
