d4b09cc45512a9ddbe60fd992127ef8d1d30519e
changnet <940477049@qq.com>
2024-12-20 17:43:43 +0800

    prepare for thread context

--- engine/src/thread/thread_message.hpp
+++ engine/src/thread/thread_message.hpp
@@ -42,7 +42,11 @@ struct ThreadMessage
     }
     void dispose()
     {
-        if (udata_) delete[] udata_;
+        if (udata_)
+        {
+            delete[] udata_;
+            udata_ = nullptr;
+        }
     }
 
     int32_t src_; // 来源地址
@@ -90,6 +94,79 @@ public:
 
 private:
     std::mutex mutex_;
+
+    /**
+     * @brief 当前线程的消息队列
+     * deque内部采用多个大块（类似vector）内存连接的实现，保证不会出现太多内存碎片
+     * 另一种方式是做一个全局的message_pool，不过需要加锁管理
+     */
+    std::deque<ThreadMessage> queue_;
+};
+
+// 线程间交互数据及唤醒的机制
+class ThreadContext
+{
+public:
+    // 从消息队列中弹出第一个消息。如果队列为空，则抛出一个out_of_range
+    ThreadMessage pop()
+    {
+        std::lock_guard<std::mutex> lg(mutex_);
+
+        if (queue_.empty())
+        {
+            // too slow
+            // throw std::out_of_range("empty queue");
+            return ThreadMessage::Null;
+        }
+
+        // ThreadMessage只有基础类型，std::mvoe现在没啥意义
+        ThreadMessage message = std::move(queue_.front());
+        queue_.pop_front();
+
+        return message; // copy elision
+    }
+
+    // 构造一个消息并唤醒线程
+    template <typename... Args> void emplace(Args &&...args)
+    {
+        {
+            std::lock_guard<std::mutex> lg(mutex_);
+            queue_.emplace_back(std::forward<Args>(args)...);
+        }
+        cv_.notify_one();
+    }
+    void push(ThreadMessage &message)
+    {
+        {
+            std::lock_guard<std::mutex> lg(mutex_);
+            queue_.emplace_back(message);
+        }
+        cv_.notify_one();
+    }
+
+    /**
+     * @brief 唤醒等待的一条线程
+     */
+    void notify_one()
+    {
+        cv_.notify_one();
+    }
+
+    /**
+     * @brief 等待N毫秒
+     * @param ms 等待的毫秒数
+     */
+    void wait_for(int64_t ms)
+    {
+        std::unique_lock<std::mutex> ul(mutex_);
+        if (queue_.empty()) cv_.wait_for(ul, std::chrono::milliseconds(ms));
+    }
+
+private:
+
+    std::mutex mutex_;
+    // TODO C＋＋20可以wait一个atomic变量，到时优化一下
+    std::condition_variable cv_;
 
     /**
      * @brief 当前线程的消息队列
