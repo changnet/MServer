be3170980d2fde675943bc05c1c3d329ab77eac1
changnet <940477049@qq.com>
2025-05-07 21:32:30 +0800

    rewrite sql

--- engine/src/mysql/sql.cpp
+++ engine/src/mysql/sql.cpp
@@ -23,89 +23,19 @@ void Sql::library_end()
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void Sql::SqlCol::clear()
-{
-    if (value_ex_)
-    {
-        delete[] value_ex_;
-        value_ex_ = nullptr;
-    }
-}
-void Sql::SqlCol::set(const char *value, size_t size)
-{
-    size_           = size; /* 注意没加1 */
-    char *mem_value = value_;
-
-    // mysql把所有类型的数据都存为char数组，对于int、double之类的，使用value_避免内存
-    // 分配，对于text、blob之类的，只能根据大小重新分配内存
-    // TODO 是否要搞一个内存池，或者把value_的默认值再调大些?
-    if (size_ >= sizeof(value_))
-    {
-        value_ex_ = new char[size + 1];
-        mem_value = value_ex_;
-    }
-
-    // 无论何种数据类型(包括寸进制)，都统一加\0方便后面转换为int之类时可以直接atoi
-    memcpy(mem_value, value, size);
-    mem_value[size] = '\0';
-}
-
-Sql::SqlQuery::SqlQuery()
-{
-    id_      = 0;
-    size_    = 0;
-    stmt_ex_ = nullptr;
-}
-
-Sql::SqlQuery::~SqlQuery()
-{
-    if (stmt_ex_) delete[] stmt_ex_;
-
-    id_      = 0;
-    size_    = 0;
-    stmt_ex_ = nullptr;
-}
-
-void Sql::SqlQuery::clear()
-{
-    if (stmt_ex_)
-    {
-        delete[] stmt_ex_;
-        stmt_ex_ = nullptr;
-    }
-}
-
-void Sql::SqlQuery::set(int32_t id, size_t size, const char *stmt)
-{
-    id_   = id;
-    size_ = size;
-
-    char *mem_stmt = stmt_;
-    if (size >= sizeof(stmt_))
-    {
-        stmt_ex_ = new char[size + 1];
-        mem_stmt = stmt_ex_;
-    }
-    memcpy(mem_stmt, stmt, size);
-    mem_stmt[size] = 0; // 保证0结尾，因为有些地方需要打印stmt
-}
-
-void Sql::SqlResult::clear()
-{
-    for (auto &cols : rows_)
-    {
-        for (auto &col : cols) col.clear();
-    }
-}
-////////////////////////////////////////////////////////////////////////////////
 Sql::Sql()
 {
-    conn_  = nullptr;
+    const int32_t SIZE = 512 * 1024; // 大于256kb使用mmap分配
+    sql_               = nullptr;
+    builder_           = new char[SIZE];
+    builder_idx_       = 0;
+    builder_len_       = SIZE;
 }
 
 Sql::~Sql()
 {
     disconnect();
+    if (builder_) delete[] builder_;
 }
 
 bool Sql::thread_init()
@@ -120,7 +50,7 @@ void Sql::thread_end()
 
 int32_t Sql::connect(lua_State *L)
 {
-    if (conn_) return luaL_error(L, "already connected");
+    if (sql_) return luaL_error(L, "already connected");
 
     const char *host = luaL_checkstring(L, 2);
     int32_t port     = luaL_checkinteger32(L, 3);
@@ -128,58 +58,58 @@ int32_t Sql::connect(lua_State *L)
     const char *pwd = luaL_checkstring(L, 5);
     const char *db = luaL_checkstring(L, 6);
 
-    conn_ = mysql_init(nullptr);
-    if (!conn_) luaL_error(L, "mysql_init fail"); 
+    sql_ = mysql_init(nullptr);
+    if (!sql_) luaL_error(L, "mysql_init fail"); 
 
         // mysql_options的时间精度都为秒级
     uint32_t connect_timeout = 60;
     uint32_t read_timeout    = 30;
     uint32_t write_timeout   = 30;
     bool reconnect           = true;
-    if (mysql_options(conn_, MYSQL_OPT_CONNECT_TIMEOUT, &connect_timeout)
-        || mysql_options(conn_, MYSQL_OPT_READ_TIMEOUT, &read_timeout)
-        || mysql_options(conn_, MYSQL_OPT_WRITE_TIMEOUT, &write_timeout)
-        || mysql_options(conn_, MYSQL_OPT_RECONNECT, &reconnect)
-        || mysql_options(conn_, MYSQL_SET_CHARSET_NAME, "utf8mb4")
+    if (mysql_options(sql_, MYSQL_OPT_CONNECT_TIMEOUT, &connect_timeout)
+        || mysql_options(sql_, MYSQL_OPT_READ_TIMEOUT, &read_timeout)
+        || mysql_options(sql_, MYSQL_OPT_WRITE_TIMEOUT, &write_timeout)
+        || mysql_options(sql_, MYSQL_OPT_RECONNECT, &reconnect)
+        || mysql_options(sql_, MYSQL_SET_CHARSET_NAME, "utf8mb4")
         /*|| mysql_options( conn, MYSQL_INIT_COMMAND,"SET autocommit=0" ) */
     )
     {
-        return mysql_errno(conn_);
+        return mysql_errno(sql_);
     }
 
     /* CLIENT_REMEMBER_OPTIONS:Without this option, if mysql_real_connect()
      * fails, you must repeat the mysql_options() calls before trying to connect
      * again. With this option, the mysql_options() calls need not be repeated
      */
-    if (mysql_real_connect(conn_, host, usr, pwd, db, port, nullptr,
+    if (mysql_real_connect(sql_, host, usr, pwd, db, port, nullptr,
                            CLIENT_REMEMBER_OPTIONS))
     {
         return 0;
     }
 
     // 在实际应用中，允许mysql先不开启或者网络原因连接不上，不断重试
-    return mysql_errno(conn_);
+    return mysql_errno(sql_);
 }
 
 void Sql::disconnect()
 {
-    if (!conn_) return;
+    if (!sql_) return;
 
-    mysql_close(conn_);
-    conn_ = nullptr;
+    mysql_close(sql_);
+    sql_ = nullptr;
 }
 
 int32_t Sql::ping()
 {
-    return conn_ ? mysql_ping(conn_) : -1;
+    return sql_ ? mysql_ping(sql_) : -1;
 }
 
 int32_t Sql::error(lua_State *L)
 {
-    if (!conn_) return 0;
+    if (!sql_) return 0;
 
-    lua_pushinteger(L, mysql_errno(conn_));
-    lua_pushstring(L, mysql_error(conn_));
+    lua_pushinteger(L, mysql_errno(sql_));
+    lua_pushstring(L, mysql_error(sql_));
     return 2;
 }
 
@@ -189,19 +119,19 @@ int32_t Sql::real_query(const char *stmt, size_t size)
     /* Client error message numbers are listed in the MySQL errmsg.h header
      * file. Server error message numbers are listed in mysqld_error.h
      */
-    if (unlikely(mysql_real_query(conn_, stmt, (unsigned long)size)))
+    if (unlikely(mysql_real_query(sql_, stmt, (unsigned long)size)))
     {
-        int32_t e = mysql_errno(conn_);
+        int32_t e = mysql_errno(sql_);
         if (CR_SERVER_LOST != e && CR_SERVER_GONE_ERROR != e)
         {
             return e;
         }
 
         // reconnect and try again
-        if (mysql_ping(conn_)
-            || mysql_real_query(conn_, stmt, (unsigned long)size))
+        if (mysql_ping(sql_)
+            || mysql_real_query(sql_, stmt, (unsigned long)size))
         {
-            return mysql_errno(conn_);
+            return mysql_errno(sql_);
         }
     }
     return 0;
@@ -209,7 +139,7 @@ int32_t Sql::real_query(const char *stmt, size_t size)
 
 int32_t Sql::exec(lua_State *L)
 {
-    if (!conn_) return luaL_error(L, "not connect");
+    if (!sql_) return luaL_error(L, "not connect");
 
     size_t size = 0;
     const char *stmt = luaL_checklstring(L, 2, &size);
@@ -217,7 +147,7 @@ int32_t Sql::exec(lua_State *L)
     /* same as mysql_real_query,return 0 if success */
     int32_t e = real_query(stmt, size);
 
-    MYSQL_RES *res = mysql_store_result(conn_);
+    MYSQL_RES *res = mysql_store_result(sql_);
     if (res) mysql_free_result(res);
 
     lua_pushinteger(L, e);
@@ -226,7 +156,7 @@ int32_t Sql::exec(lua_State *L)
 
 int32_t Sql::query(lua_State *L)
 {
-    if (!conn_) return luaL_error(L, "not connect");
+    if (!sql_) return luaL_error(L, "not connect");
 
     size_t size      = 0;
     const char *stmt = luaL_checklstring(L, 2, &size);
@@ -241,9 +171,16 @@ int32_t Sql::query(lua_State *L)
     return row_count > 0 ? 2 : 1;
 }
 
+int32_t Sql::select(lua_State *L)
+{
+    if (!sql_) return luaL_error(L, "not connect");
+
+    // select * from table where a = 1 and b = 2
+}
+
 int32_t Sql::escape(lua_State *L)
 {
-    if (!conn_) return luaL_error(L, "not connect");
+    if (!sql_) return luaL_error(L, "not connect");
     // mysql_real_escape_string_quote();
 
     size_t size     = 0;
@@ -257,14 +194,14 @@ int32_t Sql::escape(lua_State *L)
         // 一般情况下在栈上分配的足够转义
         char buffer[8 * 1024];
         unsigned long buffer_size =
-            mysql_real_escape_string(conn_, buffer, str, size);
+            mysql_real_escape_string(sql_, buffer, str, size);
         lua_pushlstring(L, buffer, buffer_size);
     }
     else
     {
         char *buffer = new char[need_size];
         unsigned long buffer_size =
-            mysql_real_escape_string(conn_, buffer, str, size);
+            mysql_real_escape_string(sql_, buffer, str, size);
         lua_pushlstring(L, buffer, buffer_size);
 
         delete[] buffer;
@@ -272,92 +209,86 @@ int32_t Sql::escape(lua_State *L)
     return 1;
 }
 
-void Sql::fetch_result(MYSQL_RES *result, SqlResult *res)
+int32_t Sql::field_to_lua(lua_State *L, int32_t type, const char *value, size_t size)
 {
-    if (!res) return; // 部分操作不需要返回结果，如update
-
-    uint32_t num_rows   = static_cast<uint32_t>(mysql_num_rows(result));
-    uint32_t num_fields = mysql_num_fields(result);
-
-    res->ecode_    = 0;
-    res->num_rows_ = num_rows;
-    res->num_cols_ = num_fields;
-    res->fields_.resize(num_fields);
-    res->rows_.resize(num_rows);
-
-    if (0 >= num_rows) return; /* we got empty set */
-
-    uint32_t index = 0;
-    MYSQL_FIELD *field;
-    auto &fields = res->fields_;
-    while ((field = mysql_fetch_field(result)))
+    switch (type)
     {
-        assert(index < num_fields);
-        fields[index].type_ = field->type;
-        snprintf(fields[index].name_, sizeof(fields[index].name_), "%s",
-                 field->name);
-
-        ++index;
+    case MYSQL_TYPE_TINY:
+    case MYSQL_TYPE_SHORT:
+    case MYSQL_TYPE_LONG:
+    case MYSQL_TYPE_TIMESTAMP:
+    case MYSQL_TYPE_INT24:
+        lua_pushinteger(L, static_cast<LUA_INTEGER>(atoi(value)));
+        break;
+    case MYSQL_TYPE_LONGLONG: lua_pushinteger(L, atoll(value)); break;
+    case MYSQL_TYPE_FLOAT:
+    case MYSQL_TYPE_DOUBLE:
+    case MYSQL_TYPE_DECIMAL:
+        lua_pushnumber(L, static_cast<LUA_NUMBER>(atof(value)));
+        break;
+    case MYSQL_TYPE_VARCHAR:
+    case MYSQL_TYPE_TINY_BLOB:
+    case MYSQL_TYPE_MEDIUM_BLOB:
+    case MYSQL_TYPE_LONG_BLOB:
+    case MYSQL_TYPE_BLOB:
+    case MYSQL_TYPE_VAR_STRING:
+    case MYSQL_TYPE_STRING: lua_pushlstring(L, value, size); break;
+    default:
+        lua_pushnil(L);
+        ELOG("unknow mysql type:%d\n", type);
+        break;
     }
 
-    MYSQL_ROW row;
-
-    index = 0;
-    while ((row = mysql_fetch_row(result)))
-    {
-        SqlResult::SqlRow &res_row = res->rows_[index];
-        res_row.resize(num_fields);
-
-        /* mysql_fetch_lengths() is valid only for the current row of the
-         * result set
-         */
-        unsigned long *lengths = mysql_fetch_lengths(result);
-        for (uint32_t i = 0; i < num_fields; i++)
-        {
-            res_row[i].set(row[i], lengths[i]);
-        }
-
-        ++index;
-    }
+    return 0;
 }
 
-int32_t Sql::result(SqlResult *res)
-{
-    assert(conn_);
-
-    /* mysql_store_result() returns a null pointer if the statement did not
-     * return a result set (for example, if it was an INSERT statement).
-     * also returns a null pointer if reading of the result set
-     * failed. You can check whether an error occurred by checking whether
-     * mysql_error() returns a nonempty string, mysql_errno() returns nonzero
-     * it does not do any harm or cause any notable performance degradation if
-     * you call mysql_store_result() in all cases(INSERT,UPDATE ...)
-     */
-    MYSQL_RES *result = mysql_store_result(conn_);
-    if (result)
-    {
-        fetch_result(result, res);
-        mysql_free_result(result);
-        return 0; /* success */
-    }
-
-    int32_t ok = mysql_errno(conn_);
-    if (res)
-    {
-        res->ecode_    = ok;
-        res->num_cols_ = 0;
-        res->num_rows_ = 0;
-    }
-
-    return ok;
-}
-
-uint32_t Sql::get_errno()
-{
-    assert(conn_);
-    return mysql_errno(conn_);
-}
 int32_t Sql::result_to_lua(lua_State *L)
 {
+    // mysql_store_result() returns NULL if the statement did not return a result set (for example, if it was an INSERT statement), or an error occurred and reading of the result set failed.
+    // An empty result set is returned if there are no rows returned.(An empty result set differs from a null pointer as a return value.)
+    MYSQL_RES *res = mysql_store_result(sql_);
+    if (!res) return 0;
+
+    MYSQL_FIELD *fields = mysql_fetch_fields(res);
+    int32_t num_rows = static_cast<int32_t>(mysql_num_rows(res));
+    uint32_t num_fields = static_cast<int32_t>(mysql_num_fields(res));
+
+    lua_createtable(L, num_rows, 0); /* 创建数组，元素个数为num_rows */
+
+    MYSQL_ROW row;
+    int32_t index = 1; // lua table从1开始
+    while ((row = mysql_fetch_row(res)))
+    {
+        // mysql_fetch_lengths() is valid only for the current row of the result set
+        unsigned long *lengths = mysql_fetch_lengths(res);
+
+        /* 创建hash表，元素个数为num_cols */
+        lua_createtable(L, 0, num_fields);
+        for (uint32_t idx = 1; idx <= num_fields; idx++)
+        {
+            const MYSQL_FIELD &field = fields[idx];
+            lua_pushlstring(L, field.name, field.name_length);
+
+            field_to_lua(L, field.type, row[idx], lengths[idx]);
+            lua_rawset(L, -3);
+        }
+        lua_rawseti(L, -2, index);
+        index++;
+    }
+
+    mysql_free_result(res);
     return 0;
+}
+
+void Sql::builder_resize(int32_t size)
+{
+    int32_t new_size = builder_idx_ + size;
+    if (new_size < builder_len_ * 2) new_size = builder_len_ * 2;
+
+    char *builder = new char[new_size];
+    memcpy(builder, builder_, builder_idx_);
+
+    delete[] builder_;
+    builder_     = builder;
+    builder_len_ = new_size;
 }
--- engine/src/mysql/sql.hpp
+++ engine/src/mysql/sql.hpp
@@ -9,109 +9,6 @@ struct lua_State;
 class Sql
 {
 public:
-    /// 字段名字
-    struct SqlField
-    {
-        char name_[64]; /// 字段名，如果超过最大限制将被截断
-        /*
-        define in mysql_com.h
-        enum enum_field_types { MYSQL_TYPE_DECIMAL, MYSQL_TYPE_TINY,
-                    MYSQL_TYPE_SHORT,  MYSQL_TYPE_LONG,
-                    MYSQL_TYPE_FLOAT,  MYSQL_TYPE_DOUBLE,
-                    MYSQL_TYPE_NULL,   MYSQL_TYPE_TIMESTAMP,
-                    MYSQL_TYPE_LONGLONG,MYSQL_TYPE_INT24,
-                    MYSQL_TYPE_DATE,   MYSQL_TYPE_TIME,
-                    MYSQL_TYPE_DATETIME, MYSQL_TYPE_YEAR,
-                    MYSQL_TYPE_NEWDATE, MYSQL_TYPE_VARCHAR,
-                    MYSQL_TYPE_BIT,
-                    MYSQL_TYPE_TIMESTAMP2,
-                    MYSQL_TYPE_DATETIME2,
-                    MYSQL_TYPE_TIME2,
-                    MYSQL_TYPE_NEWDECIMAL=246,
-                    MYSQL_TYPE_ENUM=247,
-                    MYSQL_TYPE_SET=248,
-                    MYSQL_TYPE_TINY_BLOB=249,
-                    MYSQL_TYPE_MEDIUM_BLOB=250,
-                    MYSQL_TYPE_LONG_BLOB=251,
-                    MYSQL_TYPE_BLOB=252,
-                    MYSQL_TYPE_VAR_STRING=253,
-                    MYSQL_TYPE_STRING=254,
-                    MYSQL_TYPE_GEOMETRY=255
-        };
-        */
-        enum_field_types type_;
-    };
-
-    /// 内容
-    class SqlCol
-    {
-    public:
-        SqlCol()
-        {
-            size_     = 0;
-            value_[0] = 0;
-            value_ex_ = nullptr;
-        }
-
-        ~SqlCol()
-        {
-            if (value_ex_) delete[] value_ex_;
-
-            size_     = 0;
-            value_ex_ = nullptr;
-        }
-
-        void clear();
-        void set(const char *value, size_t size);
-        const char *get() const { return value_ex_ ? value_ex_ : value_; }
-
-    private:
-        friend class LSql;
-
-        size_t size_;
-        char value_[64];
-        char *value_ex_;
-    };
-
-    /* 查询结果 */
-    class SqlResult
-    {
-    public:
-        void clear();
-
-    private:
-        friend class Sql;
-        friend class LSql;
-        using SqlRow = std::vector<SqlCol>;
-
-        int32_t id_; /* 标记查询的id，用于回调 */
-        int32_t ecode_;
-        uint32_t num_rows_;            // 行数
-        uint32_t num_cols_;            // 列数
-        std::vector<SqlField> fields_; // 字段名
-        std::vector<SqlRow> rows_;     // 行数据
-    };
-
-    /// 查询请求
-    class SqlQuery
-    {
-    public:
-        SqlQuery();
-        ~SqlQuery();
-
-        void clear();
-        void set(int32_t id, size_t size, const char *stmt);
-        const char *get() const { return stmt_ex_ ? stmt_ex_ : stmt_; }
-
-    private:
-        friend class LSql;
-        int32_t id_;
-        size_t size_;
-        char stmt_[256];
-        char *stmt_ex_;
-    };
-
-public:
     Sql();
     virtual ~Sql();
 
@@ -190,13 +87,7 @@ public:
      */
     int32_t escape(lua_State *L);
 
-    int32_t option(); /// 设置连接参数，如编码、是否自动重连
-    int32_t connect();
     void disconnect();
-    const char *error();
-    uint32_t get_errno();
-    int32_t result(SqlResult *res);
-    int32_t query(const char *stmt, size_t size);
 
     static void library_init();
     static void library_end(); /// 释放sql库，仅在程序不再使用sql时调用
@@ -204,14 +95,30 @@ public:
 private:
     int32_t real_query(const char *stmt, size_t size);
     int32_t result_to_lua(lua_State *L);
-    void fetch_result(MYSQL_RES *result, SqlResult *res);
+    int32_t field_to_lua(lua_State *L, int32_t type, const char *value,
+                         size_t size);
+
+    void builder_resize(int32_t size);
+    template <typename... Args>
+    void builder_fmt(Args &&...args)
+    {
+        /*
+         * 从 Visual Studio 2015 和 Windows 10 中的 UCRT 开始， snprintf 不再等于 _snprintf。
+         * snprintf 行为现在符合 C99 标准。 区别在于，如果缓冲区耗尽，snprintf 会以 null 
+         * 终止缓冲区的末尾并返回所需的字符数，_snprintf 则不会以 null 终止缓冲区，并会返回 -1
+         */
+        int32_t size = builder_len_ - builder_idx_;
+        int32_t len = snprintf(builder_, size, std::forward<Args>(args)...);
+        if (len >= size)
+        {
+            builder_resize(len + 1);
+            builder_fmt(std::forward<Args>(args)...);
+        }
+    }
 
 protected:
-    MYSQL *conn_;
-
-    int32_t port_;        // 数据库端口
-    std::string host_;    // 数据库ip地址
-    std::string usr_;     // 数据库用户名
-    std::string pwd_;     // 数据库密码
-    std::string db_name_; // 数据库名
+    MYSQL *sql_;
+    char *builder_; // 把lua table构建成sql语句
+    int32_t builder_len_;
+    int32_t builder_idx_;
 };
